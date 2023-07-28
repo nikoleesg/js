b0b3259e1e93262f839eb7585853e01732840baf
Niko Lee <niko.lee@outlook.sg>
2023-07-28 23:40:00 +0800

    add fix class (modal-title) HTML Escape

--- laravel-admin/frontend.js
+++ laravel-admin/frontend.js
@@ -2,5 +2,17 @@
     Fix select box can't search in Modal
  */
 $.fn.modal.Constructor.prototype.enforceFocus = function () {};
+
 $("div[id^='grid-modal-']").removeAttribute('tabindex');
 $("div[id='modal']").removeAttribute('tabindex');
+
+
+/*
+    Fix class 'modal-title' HTML Escape
+ */
+var collection = document.getElementsByClassName('modal-title');
+
+for (var i = 0; i < collection.length; i++)
+{
+    collection[i].innerHTML = collection[i].innerHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');
+}
